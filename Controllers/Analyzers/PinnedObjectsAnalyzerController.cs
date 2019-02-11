fa4f3b640d73ea5052a318bde2473a373784c849
Mehmet Kadayifci <mkadayifci@gmail.com>
2019-02-11 11:22:39 +0300

    Pinned Objects

--- Controllers/Analyzers/PinnedObjectsAnalyzerController.cs
+++ Controllers/Analyzers/PinnedObjectsAnalyzerController.cs
@@ -1,17 +1,22 @@
 ﻿using kedi.engine.Services.Analyze;
 using kedi.engine.Services.Analyzers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace kedi.engine.Controllers.Analyzers
 {
-    public class StackTraceAnalyzerController : ApiController
+  public  class PinnedObjectsAnalyzerController : ApiController
     {
-        
-        [Route("api/analyzers/stack-trace-analyzer/{sessionId}")]
+
+        [Route("api/analyzers/pinned-objects-analyzer/{sessionId}")]
         [HttpGet]
         public IHttpActionResult Get([FromUri]string sessionId)
         {
-            var analyzer = new StackTraceAnalyzer();
+            var analyzer = new PinnedObjectAnalyzer();
             var result = analyzer.Analyze(sessionId);
 
             return Ok(result);
---
+++ Services/Analyzers/PinnedObjectAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using kedi.engine.Services.Analyze;
+using Microsoft.Diagnostics.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kedi.engine.Services.Analyzers
+{
+    public class PinnedObjectAnalyzer
+    {
+        IAnalyzeOrchestrator analyzeOrchestrator = ContainerManager.Container.Resolve<IAnalyzeOrchestrator>();
+
+        public dynamic Analyze(string sessionId)
+        {
+            List<dynamic> returnValue = new List<dynamic>();
+            ClrRuntime runtime = analyzeOrchestrator.GetRuntimeBySessionId(sessionId);
+            foreach(var handle in runtime.EnumerateHandles())
+            {
+                if (handle.IsPinned)
+                {
+                    returnValue.Add(new {
+                        ObjectAddress = handle.Object,
+                        TypeName = handle.Type.Name,
+                         handle.HandleType
+                    });
+                }
+            }
+            return returnValue;
+        }
+    }
+}
